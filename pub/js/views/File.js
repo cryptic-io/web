e51472ac6af31a7b79997d8eef2c1812209d362c
Marco Munizaga <marco.munizaga@grooveshark.com>
2012-08-05 04:24:45 -0400

    Added core js files, initial support for uploading and encrypting files, support for chunking files, demo of the ability to view media clientside

---
+++ pub/js/views/File.js
@@ -0,0 +1,40 @@
+//returns the file view
+define(["models/File"], function(FileModel){ 
+    return Backbone.View.extend({
+
+        tagName: "div",
+
+        className: "fileIn",
+
+        initialize: function(){
+            this.on("fileLoaded", this.fileLoaded);
+        },
+
+        template: _.template($('#file-input-template').html()),
+
+        render: function(){
+            this.$el.html(this.template());
+            this.fileInput = this.$el.find('#file-input')[0];
+            return this.$el;
+        },
+
+        //might want to change this to get rid of jquery completely
+        events: {
+            "change #file-input" : "loadFile",
+        },
+
+        //read the file from the input
+        loadFile: function(event){
+            var fileObj = this.fileInput.files[0];
+            this.model = new FileModel({file: fileObj});
+            model = this.model;
+            this.trigger('fileLoaded');
+        },
+
+        fileLoaded: function(){
+            console.log('file has been loaded!');
+        }
+
+
+    })
+});
