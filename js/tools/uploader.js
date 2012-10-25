0b58c9c3a97b1dfa05672782f9a94186fbb5c250
Marco Munizaga <marco.munizaga@grooveshark.com>
2012-10-25 10:33:56 -0400

    updated uploader

--- js/tools/uploader.js
+++ js/tools/uploader.js
@@ -3,23 +3,17 @@ var Uploader = function() {
 };
 
 Uploader.prototype = {
-    headers : {
-        //"Accept-Charset":"utf-8"
-    },
 
     send : function(location, arraybuffer, fileName, callback) {
         var xhr = new XMLHttpRequest();
 
         xhr.open("POST", location, true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
+        xhr.responseType = 'text'
+        xhr.onload = function() {
+            if (this.status === 200) {
                 callback(xhr.responseText);
             }
         };
-
-        for (var header in this.headers) {
-            xhr.setRequestHeader(header, this.headers[header]);
-        }
 
         // finally send the request as binary data (really an arraybuffer)
         xhr.send(arraybuffer);
