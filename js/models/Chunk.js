16b8554b152c621e7cbfed700972e176cb2e8f8c
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-01-19 22:01:42 -0500

    Forgot a comma

--- js/models/Chunk.js
+++ js/models/Chunk.js
@@ -5,7 +5,7 @@ define(['tools/uploader','tools/downloader','tools/FileSystemHandler', 'models/F
 
         defaults: {
             //This chunk uses the 1.0 version of enryption, future chunks may have different versions
-            encryptionVersion: "1.0"
+            encryptionVersion: "1.0",
 
            chunkSize: 10e6  //Specify how big the chunk should be. ******  THIS HAS TO BE DIVISBLE BY 16 ****** (the reason so that we only need pad the last chunk)
            //chunksize is 10MB
