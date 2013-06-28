a64183ae7c9277ca0b673cf2ca87b7cbe2cd4638
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-06-27 22:05:57 -0400

    added snap folder

--- snap/build.sh
+++ snap/build.sh
@@ -1,7 +1,7 @@
-config_location=js/config.js
+config_location=../js/config.js
 NEMO_LOCATION=${NEMO_LOCATION:-localhost}
 NEMO_PORT=${NEMO_PORT:-8888}
-echo "//Auto-generated config made in build.sh" > $config_location
+echo "//Auto-generated config made in snap/build.sh" > $config_location
 echo "define({" >> $config_location
 echo "NEMO_LOCATION : '$NEMO_LOCATION'" >> $config_location
 echo ", NEMO_PORT : '$NEMO_PORT'" >> $config_location
