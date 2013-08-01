4de599a7fce1089c30bae674f04ffab16c54d2a2
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-07-31 21:13:58 -0400

    cleaned up lines

--- js/apiEndPoints.js
+++ js/apiEndPoints.js
@@ -1,11 +1,11 @@
 // Define the api end points to be used throughout the code
 define({
     updateUserBlob :   "/api/updateUserBlob"
-    , createUser   :   "/api/createUser"
-    , getUserBlobs :   "/api/getUserBlobs"
-    , getFileKeys  :   "/api/getFileKeys"
-    , uploadFile   :   "/api/uploadFile"
-    , anonUploadFile : "/api/anonUploadFile"
-    , removeFile   :   "/api/removeFiles"
-    , multipass    :   "/api/getMultipass"
+  , createUser     :   "/api/createUser"
+  , getUserBlobs   :   "/api/getUserBlobs"
+  , getFileKeys    :   "/api/getFileKeys"
+  , uploadFile     :   "/api/uploadFile"
+  , anonUploadFile :  "/api/anonUploadFile"
+  , removeFile     :   "/api/removeFiles"
+  , multipass      :   "/api/getMultipass"
 })
