ccaeb981f57eae9f012e862fea9437b6d38682cb
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-09-05 13:18:43 -0400

    Added the logic to handle sending emails, emails not actually sent yet.

--- js/views/About.js
+++ js/views/About.js
@@ -4,6 +4,33 @@ define(["jade!templates/About"], function(template, UserBlob){
         id : "aboutContainer",
         className : "floatingContainer",
 
+        events: {
+          "click #subscribeEmail":"saveEmail",
+          "keypress #contactEmail":"checkForEnter"
+        },
+
+        saveEmail: function(){
+          var email = $("#contactEmail").val()
+
+          // save email code
+
+          //update the page
+          $("#contactEmail").parent().addClass("success")
+          $("#contactEmail").siblings("label").removeClass("icon-envelope")
+          $("#contactEmail").siblings("label").addClass("icon-check-sign")
+          $("#contactEmail").siblings("label").css("color", "#2ECC71")
+          $("#contactEmail").val("Done, and Done")
+          $("#subscribeEmail").remove()
+
+        },
+
+        checkForEnter: function(){
+          var enterKey = 13;
+          if (event.which === enterKey){
+            this.saveEmail()
+          }
+        },
+
         render: function() {
             this.$el.html(template());
         },
