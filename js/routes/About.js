750c060457305e0d77a6012a8f03aadf47e5a996
Marco Munizaga <marco.munizaga@grooveshark.com>
2013-12-12 14:28:12 -0500

    added new about page

--- js/routes/About.js
+++ js/routes/About.js
@@ -1,16 +1,7 @@
 define([ "views/About" ], function(About){
   return function(viewport, topBar){
     return function(){
-      console.log('Entering About')
-      var about = new About()
-      about.render()
-      topBar.select('about')
-
-      viewport
-      .exeunt()
-      .introduce(about,0)
-      .moveToPage(0)
-      .placeCenter(about.el, 0)
+      window.location.pathname = "/about"
     }
   }
 })
